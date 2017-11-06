e9187072e8f71bc4fd55461c94d8fd4b945e5b40
Stefanny <stefanny@gmail.com>
2017-11-06 11:16:28 -0500

    First commit

---
+++ labSpringMVCTesting/src/main/java/com/tecsup/gestion/services/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.tecsup.gestion.services;
+import java.util.List;
+
+import com.tecsup.gestion.exception.DAOException;
+import com.tecsup.gestion.exception.EmptyResultException;
+import com.tecsup.gestion.model.Employee;
+
+public interface EmployeeService {
+	
+	Employee find(int employee_id) throws DAOException, EmptyResultException;
+	List<Employee> findAll() throws DAOException, EmptyResultException;
+
+
+}
+
